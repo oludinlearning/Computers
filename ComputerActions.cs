fb6d26f32859b65b42b55fcc4627d69f25d670c2
Алексей Олудин <oludin.al@outlook.com>
2019-10-19 15:10:51 +0500

    Добавлены интерфейсы

---
+++ ComputerActions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computers
+{
+    class ComputerActions : IComputerActions
+    {
+        void IComputerActions.Start(Computers computer)
+        {
+            if (computer.powerOn) {
+                computer.osOn = true;
+            }
+        }
+        void IComputerActions.Shutdown(Computers computer)
+        {
+            if (computer.powerOn) {
+                computer.osOn = false;
+            }
+        }
+        void IComputerActions.Restart(Computers computer)
+        {
+            IComputerActions tempAction = new ComputerActions();
+            if (computer.osOn)
+            {
+                tempAction.Shutdown(computer);
+                tempAction.Start(computer);
+            }
+        }
+        void IComputerActions.PowerOn(Computers computer)
+        {
+            if (!computer.powerOn)
+            {
+                computer.powerOn = true;
+            }
+        }
+        void IComputerActions.PowerOff(Computers computer)
+        {
+            if (computer.powerOn)
+            {
+                computer.powerOn = false;
+            }
+        }
+        void IComputerActions.PushStartButton(Computers computer)
+        {
+            IComputerActions tempAction = new ComputerActions();
+            if (!computer.powerOn)
+            {
+                tempAction.Start(computer);
+            }
+            else
+            {
+                tempAction.Shutdown(computer);
+            }
+        }
+    }
+}
---
+++ IComputerActions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computers
+{
+    interface IComputerActions
+    {
+        void Start(Computers computer);
+        void Shutdown(Computers computer);
+        void Restart(Computers computer);
+        void PowerOn(Computers computer);
+        void PowerOff(Computers computer);
+        void PushStartButton(Computers computer);
+    }
+}
