87765b48a738c9f2a080e745008695c8d2a2e4aa
Алексей Олудин <oludin.al@outlook.com>
2019-10-25 01:13:27 +0500

    Загрузка данных из модели и отображение их в форме при переключение радиокнопок (Ноутбук, Компьютер, Сервер)

--- IMainView.cs
+++ IMainView.cs
@@ -9,6 +9,8 @@ namespace Computers
     interface IMainView
     {
         void SetComputerType(string type);
+        void SetComputerPowerstate(string powerstate);
+        void SetComputerOSState(string osstate);
         event MainView.RadioChecked RadioNotebook_Checked;
         event MainView.RadioChecked RadioPC_Checked;
         event MainView.RadioChecked RadioServer_Checked;
